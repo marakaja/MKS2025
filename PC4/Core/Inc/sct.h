830a2e6aac79b47fd5bb07c9eba27bd4474eaf75
Marek Karlíček <marekkarlicek@gmail.com>
2025-10-15 08:44:39 +0200

    Working ADC, segments and bargraph

---
+++ PC4/Core/Inc/sct.h
@@ -0,0 +1,21 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 8, 2025
+ *      Author: Mara
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include <stdint.h>
+
+// Pinout SCT_NOE_Pin | SCT_CLK_Pin | SCT_SDI_Pin | SCT_NLA_Pin
+// Sense on rising edge of clock
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t led);
+uint32_t get_bar_mask(uint8_t level);
+
+#endif /* INC_SCT_H_ */
